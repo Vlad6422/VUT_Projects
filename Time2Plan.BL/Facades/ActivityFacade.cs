cc6d62359e640777ccd7917678b821f43c3e9022
Václav Berný <xberny00@vutbr.cz>
2023-04-07 00:55:43 +0200

    Implemented filtering of activities in list model.

--- Time2Plan.BL/Facades/ActivityFacade.cs
+++ Time2Plan.BL/Facades/ActivityFacade.cs
@@ -1,4 +1,7 @@
-﻿using Time2Plan.BL.Facades.Interfaces;
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Time2Plan.BL.Facades.Interfaces;
 using Time2Plan.BL.Mappers;
 using Time2Plan.BL.Mappers.Interfaces;
 using Time2Plan.BL.Models;
@@ -12,5 +15,19 @@ public class ActivityFacade : FacadeBase<ActivityEntity, ActivityListModel, Acti
 {
     public ActivityFacade(IUnitOfWorkFactory unitOfWorkFactory,IActivityModelMapper modelMapper) : base(unitOfWorkFactory, modelMapper)
     {
+    }
+    public virtual async Task<IEnumerable<ActivityListModel>> GetAsyncFilter(ActivityListModel model, Guid projectId, DateTime fromDate, DateTime toDate, string tag, ProjectEntity project)
+    {
+        await using IUnitOfWork uow = UnitOfWorkFactory.Create();
+
+        IQueryable<ActivityEntity> query = uow.GetRepository<ActivityEntity, ActivityEntityMapper>().Get();
+
+        List<ActivityEntity> entities = await query
+            .Where(e => e.Start > fromDate)
+            .Where(e => e.Start < toDate)
+            .Where (e => e.Project == project)
+            .ToListAsync();
+
+        return ModelMapper.MapToListModel(entities);
     }
 }
