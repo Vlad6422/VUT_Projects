9460d61a9799868b70328f3c1773b012394f7e5b
Vladyslav Malashchuk <vladok.malashuk@gmail.com>
2023-04-24 20:54:10 +0200

    Need to write ViewModels... AutorithationPage must show Users....

--- Time2Plan.App/ViewModels/User/UserListViewModel.cs
+++ Time2Plan.App/ViewModels/User/UserListViewModel.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using Time2Plan.App.Services;
+using Time2Plan.BL.Facades;
+using Time2Plan.BL.Facades.Interfaces;
+using Time2Plan.BL.Models;
 
-namespace Time2Plan.App.ViewModels.User
-{
-    class UserListViewModel
+
+namespace Time2Plan.App.ViewModels;
+
+    public partial class UserListViewModel : ViewModelBase
     {
+    private readonly IUserFacade _userFacade;
+    private readonly INavigationService _navigationService;
+    public IEnumerable<UserListModel> Users { get; set; } = null!;
+    public UserListViewModel(
+        IUserFacade userFacade,
+        INavigationService navigationService,
+        IMessengerService messengerService) 
+        : base(messengerService)
+    {
+        _userFacade= userFacade;
+        _navigationService= navigationService;
     }
+    protected override async Task LoadDataAsync()
+    {
+        await base.LoadDataAsync();
+
+        Users = await _userFacade.GetAsync();
+    }
+    
+
 }
+
