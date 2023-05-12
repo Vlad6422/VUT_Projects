87d81c3decaee9adb56656a961116b8b974e91d2
Lukas Duka <xdukal00@vutbr.cz>
2023-05-12 19:35:53 +0200

    Change structure of user login frame

--- Time2Plan.DAL/Seeds/UserSeeds.cs
+++ Time2Plan.DAL/Seeds/UserSeeds.cs
@@ -11,10 +11,28 @@ public static class UserSeeds
 {
     public static readonly UserEntity StepanUser = new()
     {
-        Id = Guid.Parse("fabde0cd-eefe-443f-baf6-3d96cc2cbf2e"),
+        Id = Guid.Parse("fabde0cd-eefe-443f-baf6-3746cc2cbf2e"),
         Name = "Stepan",
-        Surname = "BasketbollMaster",
-        NickName = "Stepan123"
+        Surname = "Ukazkovy",
+        NickName = "StepanTheBeast"
+    };
+
+    public static readonly UserEntity AnnaUser = new()
+    {
+        Id = Guid.Parse("fabde0cd-eefe-443f-baf6-3d96cc2cbf2e"),
+        Name = "Anna",
+        Surname = "Stein",
+        NickName = "AnduleStainka",
+        Photo = "https://www.flera.cz/wp-content/uploads/2018/11/anna-chomjakova.jpg"
+    };
+
+    public static readonly UserEntity PatrickUser = new()
+    {
+        Id = Guid.Parse("fabde0cd-eefe-443f-baf6-3d96bb2cbf2e"),
+        Name = "Patricius",
+        Surname = "Robinson",
+        NickName = "UserAgent007",
+        Photo = "https://nickelodeonuniverse.com/wp-content/uploads/Patrick.png"
     };
     static UserSeeds()
     {
@@ -22,6 +40,8 @@ public static class UserSeeds
     }
     public static void Seed(this ModelBuilder modelBuilder) =>
         modelBuilder.Entity<UserEntity>().HasData(
-            StepanUser 
+            StepanUser, 
+            AnnaUser,
+            PatrickUser
         );
 }
