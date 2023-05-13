32a8ad25ba14bb70e519422428d351a195f242b7
Vladyslav Malashchuk <vladok.malashuk@gmail.com>
2023-05-13 15:12:42 +0200

    Add ActivityListViewModel

---
+++ Time2Plan.App/ViewModels/Activity/ActivityListViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Time2Plan.App.Messages;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using Time2Plan.App.Services;
+using Time2Plan.BL.Facades;
+using Time2Plan.BL.Models;
+
+namespace Time2Plan.App.ViewModels;
+
+public partial  class ActivityListViewModel : ViewModelBase, IRecipient<ActivityEditMessage>,IRecipient<ActivityDeleteMessage>
+{
+    private readonly IActivityFacade _activityFacade;
+    private readonly INavigationService _navigationService;
+
+    public IEnumerable<ActivityListModel> Activitys { get; set; } = null!;
+    public ActivityListViewModel(
+       IActivityFacade ingredientFacade,
+       INavigationService navigationService,
+       IMessengerService messengerService)
+       : base(messengerService)
+    {
+        _activityFacade = ingredientFacade;
+        _navigationService = navigationService;
+    }
+
+    protected override async Task LoadDataAsync()
+    {
+        await base.LoadDataAsync();
+
+        Activitys = await _activityFacade.GetAsync();
+    }
+   /* [RelayCommand]
+    private async Task GoToCreateAsync()
+    {
+        await _navigationService.GoToAsync("/edit");
+    }
+
+    [RelayCommand]
+    private async Task GoToDetailAsync(Guid id)
+    {
+        await _navigationService.GoToAsync<ActivityDetailViewModel>(
+            new Dictionary<string, object?> { [nameof(ActivityDetailViewModel.Id)] = id });
+    }*/
+
+    public async void Receive(ActivityEditMessage message)
+    {
+        await LoadDataAsync();
+    }
+
+    public async void Receive(ActivityDeleteMessage message)
+    {
+        await LoadDataAsync();
+    }
+
+}
