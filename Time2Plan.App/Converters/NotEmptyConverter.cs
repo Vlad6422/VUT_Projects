ab2afdbe0e8a1b09a765e8587ee733a1f95fad07
Vladyslav Malashchuk <vladok.malashuk@gmail.com>
2023-05-16 10:00:31 +0200

    Added Exeption to Button,now cannot add Empty User,Button will be IsEnable=false

---
+++ Time2Plan.App/Converters/NotEmptyConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time2Plan.App.Converters;
+
+public class NotEmptyConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string str)
+        {
+            return !string.IsNullOrEmpty(str);
+        }
+
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Time2Plan.App/Views/User/UserEditView.xaml.cs
+++ Time2Plan.App/Views/User/UserEditView.xaml.cs
@@ -1,5 +1,5 @@
 using Time2Plan.App.ViewModels;
-
+using Time2Plan.App.Converters;
 namespace Time2Plan.App.Views.User;
 
 public partial class UserEditView
@@ -7,6 +7,8 @@ public partial class UserEditView
     public UserEditView(UserEditViewModel viewModel)
         : base(viewModel)
     {
+        Resources.Add("NotEmptyConverter", new NotEmptyConverter());
+
         InitializeComponent();
     }
 }
