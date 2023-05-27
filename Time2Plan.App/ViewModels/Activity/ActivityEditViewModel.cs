52b6392787ec1c994cf7391bbc1b52bdf332ea30
JakubSicner <xsicne03@vutbr.cz>
2023-05-27 22:49:18 +0200

    Fixed Date showing

--- Time2Plan.App/ViewModels/Activity/ActivityEditViewModel.cs
+++ Time2Plan.App/ViewModels/Activity/ActivityEditViewModel.cs
@@ -17,11 +17,11 @@ public partial class ActivityEditViewModel : ViewModelBase, INotifyPropertyChang
 
     public string UserId { get; set; }
     public ActivityDetailModel Activity { get; init; } = ActivityDetailModel.Empty;
-    public DateTime EndDate { get; set; } = DateTime.UtcNow;
-    public TimeSpan EndTime { get; set; } = DateTime.UtcNow.TimeOfDay;
+    public DateTime EndDate { get; set; } = DateTime.Now;
+    public TimeSpan EndTime { get; set; } = DateTime.Now.TimeOfDay;
 
-    public DateTime StartDate { get; set; } = DateTime.UtcNow; 
-    public TimeSpan StartTime { get; set; } = DateTime.UtcNow.TimeOfDay;
+    public DateTime StartDate { get; set; } = DateTime.Now; 
+    public TimeSpan StartTime { get; set; } = DateTime.Now.TimeOfDay;
 
     public ActivityEditViewModel(
         IActivityFacade activityFacade,
@@ -38,8 +38,8 @@ public partial class ActivityEditViewModel : ViewModelBase, INotifyPropertyChang
     [RelayCommand]
     private async Task SaveAsync()
     {
-        Activity.Start = StartDate.Add(StartTime);
-        Activity.End = EndDate.Add(EndTime);
+        Activity.Start = StartDate.Date.Add(StartTime);
+        Activity.End = EndDate.Date.Add(EndTime);
         if (Activity.Start >= Activity.End)
         {
             await _alertService.DisplayAsync("Invalid Time","Start time is same or greater then end time.");
