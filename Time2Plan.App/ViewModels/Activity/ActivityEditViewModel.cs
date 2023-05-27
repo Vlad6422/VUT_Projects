1528bc38587f8b36174426e048931c6be6e79398
JakubSicner <xsicne03@vutbr.cz>
2023-05-27 22:39:23 +0200

    Invalid Date treated by alert services

--- Time2Plan.App/ViewModels/Activity/ActivityEditViewModel.cs
+++ Time2Plan.App/ViewModels/Activity/ActivityEditViewModel.cs
@@ -13,23 +13,26 @@ public partial class ActivityEditViewModel : ViewModelBase, INotifyPropertyChang
 {
     private readonly IActivityFacade _activityFacade;
     private readonly INavigationService _navigationService;
+    private readonly IAlertService _alertService;
 
     public string UserId { get; set; }
     public ActivityDetailModel Activity { get; init; } = ActivityDetailModel.Empty;
-    public DateTime EndDate { get; set; } = DateTime.Today.AddHours(1);
-    public TimeSpan EndTime { get; set; }
+    public DateTime EndDate { get; set; } = DateTime.UtcNow;
+    public TimeSpan EndTime { get; set; } = DateTime.UtcNow.TimeOfDay;
 
-    public DateTime StartDate { get; set; } = DateTime.Today; 
-    public TimeSpan StartTime { get; set; }
+    public DateTime StartDate { get; set; } = DateTime.UtcNow; 
+    public TimeSpan StartTime { get; set; } = DateTime.UtcNow.TimeOfDay;
 
     public ActivityEditViewModel(
         IActivityFacade activityFacade,
         INavigationService navigationService,
-        IMessengerService messengerService)
+        IMessengerService messengerService,
+        IAlertService alertService)
         : base(messengerService)
     {
         _activityFacade = activityFacade;
         _navigationService = navigationService;
+        _alertService = alertService;
     }
 
     [RelayCommand]
@@ -37,6 +40,11 @@ public partial class ActivityEditViewModel : ViewModelBase, INotifyPropertyChang
     {
         Activity.Start = StartDate.Add(StartTime);
         Activity.End = EndDate.Add(EndTime);
+        if (Activity.Start >= Activity.End)
+        {
+            await _alertService.DisplayAsync("Invalid Time","Start time is same or greater then end time.");
+            return;
+        }
         await _activityFacade.SaveAsync(Activity);
         MessengerService.Send(new ActivityEditMessage { ActivityId = Activity.Id });
         _navigationService.SendBackButtonPressed();
