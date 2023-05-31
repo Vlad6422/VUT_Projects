69577abeef0139749f8d2ab4e70b518d11e91254
Lukas Duka <xdukal00@vutbr.cz>
2023-05-31 23:07:49 +0200

    Added NickName check for users

--- Time2Plan.App/ViewModels/User/UserEditViewModel.cs
+++ Time2Plan.App/ViewModels/User/UserEditViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows.Forms;
 using Time2Plan.App.Messages;
 using Time2Plan.App.Services;
 using Time2Plan.BL.Facades;
@@ -11,24 +12,46 @@ public partial class UserEditViewModel : ViewModelBase
 {
     private readonly IUserFacade _UserFacade;
     private readonly INavigationService _navigationService;
+    private readonly IAlertService _alertService;
 
     public UserDetailModel User { get; init; } = UserDetailModel.Empty;
+    public IEnumerable<UserListModel> Users { get; set; }
 
     public UserEditViewModel(
         IUserFacade UserFacade,
         INavigationService navigationService,
-        IMessengerService messengerService)
+        IMessengerService messengerService,
+        IAlertService alertService)
         : base(messengerService)
     {
         _UserFacade = UserFacade;
         _navigationService = navigationService;
+        _alertService = alertService;
     }
 
     [RelayCommand]
     private async Task SaveAsync()
     {
-        await _UserFacade.SaveAsync(User);
-        MessengerService.Send(new UserEditMessage { UserId = User.Id });
-        _navigationService.SendBackButtonPressed();
+        if(await CheckNicknames(User.NickName))
+        {
+            await _UserFacade.SaveAsync(User);
+            MessengerService.Send(new UserEditMessage { UserId = User.Id });
+            _navigationService.SendBackButtonPressed();
+        }
+    }
+
+    public async Task<bool> CheckNicknames(string newNickname)
+    {
+        Users = await _UserFacade.GetAsync();
+
+        foreach (var user in Users)
+        {
+            if (user.NickName == newNickname)
+            {
+                await _alertService.DisplayAsync("Create user failed", "Another user with nickname '" + user.NickName + "' already exists.");
+                return false;
+            }
+        }
+        return true;
     }
 }
